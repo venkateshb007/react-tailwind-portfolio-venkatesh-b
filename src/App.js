c017e3e96158245d526a8b0efa44ae4b3b6b1ef3
Venkateshb007 <venkateshb698@gmail.com>
2023-07-21 13:18:08 +0530

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>My portfolio</h1>
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
 
 export default App;
